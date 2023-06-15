1d6f0ff3250bec868630cda2824fc505bbd6ea39
rime <s1337228@yandex.ru>
2023-06-15 03:15:17 +0300

    schedule component preview

--- frontend/src/Components/Schedule/Schedule.tsx
+++ frontend/src/Components/Schedule/Schedule.tsx
@@ -1,12 +1,59 @@
 import s from './Schedule.module.scss';
 
 const Schedule = () => {
-	console.log(new Date());
+
+	const separator = (
+		<div className={s.separator}/>
+	)
+
+	const darkDots = (
+		<div className={s.darkDots}>
+			<div className={s.darkDot}/>
+			<div className={s.darkDot}/>
+		</div>
+	)
+
+	const redTab = <div className={s.redTab}/>
+
+	const event = (
+		<div className={s.event}>
+			<div className={s.time}>
+				<div className={s.timeRange}>00:00{redTab}08:30</div>
+				<div className={s.timeTaken}>8h 30m</div>
+			</div>
+			{darkDots}
+			<div className={s.description}>
+				vava vivaasdasd
+			</div>
+		</div>
+	)
+
+	const day = (
+		<div className={s.dayInner}>
+			<div className={s.date}>
+				13
+			</div>
+			<div className={s.events}>
+				{event}
+				{separator}
+				{event}
+				{separator}
+				{event}
+				{separator}
+				{event}
+				{separator}
+				{event}
+			</div>
+		</div>
+	)
 
 	return (
-		<div>
+		<div className={s.schedule}>
 			<div className={s.dayNames}>
 				{dayNames.map(x=><span key={x}>{x}</span>)}
+			</div>
+			<div className={s.dayObjects}>
+				{Array(42).fill(day).map((v,i) => <div key={i} className={s.day}>{v}</div> )}
 			</div>
 		</div>
 	)
